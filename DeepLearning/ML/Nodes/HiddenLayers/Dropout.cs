7edbfe197a0ac0cd94f6ba643f6fbc5021eabfc9
Rick <rick1304.25@gmail.com>
2023-12-03 19:00:14 -0600

    Agregación de comentarios y modificación de funciones.

--- DeepLearning/ML/Nodes/HiddenLayers/Dropout.cs
+++ DeepLearning/ML/Nodes/HiddenLayers/Dropout.cs
@@ -1,64 +1,86 @@
 ﻿using DeepLearning.ML.Optimizers;
 
-namespace DeepLearning.ML.Nodes.HiddenLayers;
-
-/// <summary>
-/// Capa oculta que implementa Dropout.
-/// </summary>
-public class Dropout : HiddenLayer
+namespace DeepLearning.ML.Nodes.HiddenLayers
 {
-    private readonly double _dropoutRate;
-    private Random _random;
-    private bool[] _dropoutMask;
-
     /// <summary>
-    /// Constructor de Dropout.
+    /// Capa oculta que implementa Dropout.
     /// </summary>
-    /// <param name="numOutputs">Número de salidas.</param>
-    /// <param name="optimizer">Optimizador.</param>
-    /// <param name="dropoutRate">Tasa de Dropout (0 a 1).</param>
-    public Dropout(int numOutputs, Optimizer optimizer, double dropoutRate) 
-        : base(numOutputs, optimizer)
+    public class Dropout : HiddenLayer
     {
-        _dropoutRate = dropoutRate;
-        _random = new Random();
-    }
+        // Tasa de Dropout
+        private readonly double _dropoutRate;
+        // Generador de números aleatorios
+        private readonly Random _random;
+        // Máscara de dropout
+        private bool[] _dropoutMask;
 
-    protected override double[,] ActivFunc(double[,] preActivation, bool trainingMode)
-    {
-        // Durante el entrenamiento, aplica Dropout
-        if (trainingMode)
+        /// <summary>
+        /// Constructor de la clase Dropout.
+        /// Inicializa la tasa de Dropout y el generador de números aleatorios.
+        /// </summary>
+        /// <param name="numOutputs">Número de salidas de la capa.</param>
+        /// <param name="optimizer">Optimizador a utilizar.</param>
+        /// <param name="dropoutRate">Tasa de Dropout (0 a 1).</param>
+        public Dropout(int numOutputs, Optimizer optimizer, double dropoutRate) 
+            : base(numOutputs, optimizer)
         {
-            var size = preActivation.GetLength(1);
-            _dropoutMask = new bool[size];
-            for (var i = 0; i < size; i++)
+            _dropoutRate = dropoutRate;
+            _random = new Random();
+        }
+
+        /// <summary>
+        /// Función de activación aplicada a la capa.
+        /// Durante el entrenamiento, aplica Dropout a los nodos de la capa.
+        /// </summary>
+        /// <param name="preActivation">Valores de pre-activación de la capa.</param>
+        /// <param name="trainingMode">Indica si la red está en modo de entrenamiento.</param>
+        /// <returns>Valores de activación con Dropout aplicado.</returns>
+        protected override double[,] ActivFunc(double[,] preActivation, bool trainingMode)
+        {
+            // Aplica Dropout solo durante el entrenamiento
+            if (trainingMode)
             {
-                _dropoutMask[i] = _random.NextDouble() > _dropoutRate;
-                if (_dropoutMask[i])
+                var size = preActivation.GetLength(1);
+                _dropoutMask = new bool[size];
+                for (var i = 0; i < size; i++)
                 {
-                    for (var j = 0; j < preActivation.GetLength(0); j++)
+                    // Determina aleatoriamente si el nodo se "apaga"
+                    _dropoutMask[i] = _random.NextDouble() > _dropoutRate;
+                    if (_dropoutMask[i])
                     {
-                        preActivation[j, i] *= _dropoutMask[i] ? 1.0 : 0.0;
+                        // Aplica el efecto de Dropout a los nodos activos
+                        for (var j = 0; j < preActivation.GetLength(0); j++)
+                        {
+                            preActivation[j, i] *= _dropoutMask[i] ? 1.0 : 0.0;
+                        }
                     }
                 }
             }
+            return preActivation;
         }
-        return preActivation;
-    }
 
-    protected override double[,] DerivFunc(double[,] preActivation)
-    {
-        // Durante el entrenamiento, aplica la máscara de Dropout
-        for (var i = 0; i < preActivation.GetLength(1); i++)
+        /// <summary>
+        /// Derivada de la función de activación.
+        /// Durante el entrenamiento, aplica la máscara de Dropout a la derivada.
+        /// </summary>
+        /// <param name="preActivation">Valores de pre-activación de la capa.</param>
+        /// <returns>Derivada de la función de activación con Dropout aplicado.</returns>
+        protected override double[,] DerivFunc(double[,] preActivation)
         {
-            if (!_dropoutMask[i])
+            var size = new[]{ preActivation.GetLength(0), preActivation.GetLength(1) };
+            // Aplica la máscara de Dropout a la derivada
+            for (var i = 0; i < size[0]; i++)
             {
-                for (var j = 0; j < preActivation.GetLength(0); j++)
+                if (!_dropoutMask[i])
                 {
-                    preActivation[j, i] = 0.0;
+                    // "Apaga" los nodos que fueron desactivados durante la activación
+                    for (var j = 0; j < size[1]; j++)
+                    {
+                        preActivation[j, i] = 0.0;
+                    }
                 }
             }
+            return preActivation;
         }
-        return preActivation;
     }
 }
